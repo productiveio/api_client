b78baf4e505e1e5a33c303ad8890dd16a1b6ad39
bjosip <josip@infinum.hr>
2016-08-03 17:56:30 +0200

    updated generator

--- lib/generators/productive/productive_generator.rb
+++ lib/generators/productive/productive_generator.rb
@@ -6,26 +6,24 @@ class ProductiveGenerator < Rails::Generators::Base
   argument :api_key, required: true, desc: 'required'
   argument :account_id, required: true, desc: 'required'
 
-  gem 'productive'
-
   desc 'Configure Productive API client'
 
   def create_initializer_file
-    unless /^[a-f0-9\-]{36}$/ =~ api_key
+    if /^[a-f0-9\-]{36}$/ !~ api_key
       raise Thor::Error, "Invalid Productive API key #{api_key.inspect}\nYou can find the API key on the Settings -> Security page of https://productive.io"
     end
 
-    unless /^[0-9]*$/ =~ account_id
+    if /^[0-9]*$/ !~ account_id
       raise Thor::Error, "Invalid Productive Account ID #{account_id.inspect}\n"
     end
 
     initializer 'productive.rb' do
-      <<-EOF
-Productive.configure do |config|
-  config.api_key = #{api_key.inspect}
-  config.account_id = #{account_id.inspect}
-end
-      EOF
+      <<-CONFIG.strip_heredoc
+        Productive.configure do |config|
+          config.api_key = #{api_key.inspect}
+          config.account_id = #{account_id.inspect}
+        end
+      CONFIG
     end
   end
 end
