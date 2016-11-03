197a837fed9872ebfa0f593f417a01ec395ed8ee
ivan <ivan.grgurevic@infinum.hr>
2016-11-03 11:52:31 +0100

    Add organization_membership resource

--- lib/productive/resources/user.rb
+++ lib/productive/resources/user.rb
@@ -1,5 +1,6 @@
 module Productive
   class User < Base
     has_one :default_organization, class_name: :organization
+    has_one :default_organization_membership, class_name: :organization_membership
   end
 end
