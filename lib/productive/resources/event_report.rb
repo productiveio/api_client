b53f14918e90c053a2476db75779134ba148c849
meshin <zeljko.mesin@productive.io>
2018-05-18 15:39:52 +0200

    adds event child on EventReport resource

--- lib/productive/resources/event_report.rb
+++ lib/productive/resources/event_report.rb
@@ -1,5 +1,6 @@
 module Productive
   class EventReport < BaseAccount
     has_one :person
+    has_one :event
   end
 end
--- lib/productive/version.rb
+++ lib/productive/version.rb
@@ -1,3 +1,3 @@
 module Productive
-  VERSION = '0.6.10'.freeze
+  VERSION = '0.6.11'.freeze
 end
