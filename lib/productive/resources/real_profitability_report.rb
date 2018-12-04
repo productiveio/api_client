e03ad0263b1835972d71790568f33ea8b4311f8f
Ivan Lučin <lucin.ivan@gmail.com>
2018-12-04 09:45:46 +0100

    Add real profitability report resource

---
+++ lib/productive/resources/real_profitability_report.rb
@@ -0,0 +1,9 @@
+module Productive
+  class ProfitabilityReport < BaseAccount
+    has_one :deal
+    has_one :company
+    has_one :project
+    has_one :manager, class_name: :person
+    has_one :service_type
+  end
+end
