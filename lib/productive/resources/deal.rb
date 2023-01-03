c98133647e5b7c4921d13149cc6105dae4240dd0
Andy Pfister <andy.pfister@simplificator.com>
2023-01-03 10:17:55 +0100

    Add custom endpoints for deals
    According to the developer documentation of Productive, deals have three custom methods, `open`, `close` and `convert_to_budget`.
    
    To make use of those, they need to be mapped in the resource class. Afterward, it is possible to e.g. call `Productive::Deal.find(9873426948).first.close`.

--- lib/productive/resources/deal.rb
+++ lib/productive/resources/deal.rb
@@ -2,5 +2,9 @@ module Productive
   class Deal < BaseAccount
     has_one :deal_status
     has_one :lost_reason
+
+    custom_endpoint :open, on: :member, request_method: :put
+    custom_endpoint :close, on: :member, request_method: :put
+    custom_endpoint :convert_to_budget, on: :member, request_method: :put
   end
 end
