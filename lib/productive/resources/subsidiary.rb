c9d0b67933a67b6fe8eb6a1c973997830740bf04
meshin <zeljko.mesin@productive.io>
2024-06-26 13:47:55 +0200

    [update] subsidiary custom domain relationship

--- lib/productive/resources/subsidiary.rb
+++ lib/productive/resources/subsidiary.rb
@@ -2,5 +2,6 @@ module Productive
   class Subsidiary < BaseAccount
     has_one :organization
     has_one :bill_from, class_name: :contact_entry
+    has_one :custom_domain
   end
 end
--- lib/productive/version.rb
+++ lib/productive/version.rb
@@ -1,3 +1,3 @@
 module Productive
-  VERSION = '0.6.77'.freeze
+  VERSION = '0.6.78'.freeze
 end
